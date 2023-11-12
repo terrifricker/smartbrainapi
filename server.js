0e0ec9ce2a0ac3e05c099af3f58e9b50eefa8215
Terri Fricker <51532684+terrifricker@users.noreply.github.com>
2023-11-12 11:26:15 -0800

    switch John and Sally to make Sally users[0]

--- server.js
+++ server.js
@@ -12,21 +12,20 @@ const database = {
     users: [
         {
             id: 123,
-            name: 'John',
-            email: 'john@gmail.com',
-            password: 'cookies',
-            entries: 0,
-            joined: new Date()
-        },
-        {
-            id: 124,
             name: 'Sally',
             email: 'sally@gmail.com',
             password: 'bananas',
             entries: 0,
             joined: new Date()
+        },
+        {
+            id: 124,
+            name: 'John',
+            email: 'john@gmail.com',
+            password: 'cookies',
+            entries: 0,
+            joined: new Date()
         }
-
     ],
     login: [
         {
